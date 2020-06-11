c1443faa44aa5729e714624454f998bf3b8df328
Jonathan Tse <Jonathan@Jonathans-MBP.connect>
2020-06-11 10:38:27 +0100

    Updates

---
+++ precache-manifest.49de68f2e1ee15f4122efa3e15430d46.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba17816c944372f0ba1fce1b24a9b57f",
+    "url": "/todo-react-app/index.html"
+  },
+  {
+    "revision": "8c8c9a3ce698bdcaf7f9",
+    "url": "/todo-react-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f45b55b0f879fe56d7ae",
+    "url": "/todo-react-app/static/js/2.bd016598.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-react-app/static/js/2.bd016598.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c8c9a3ce698bdcaf7f9",
+    "url": "/todo-react-app/static/js/main.094458d5.chunk.js"
+  },
+  {
+    "revision": "5c8a48a302b18e30ba85",
+    "url": "/todo-react-app/static/js/runtime-main.380388a8.js"
+  }
+]);
